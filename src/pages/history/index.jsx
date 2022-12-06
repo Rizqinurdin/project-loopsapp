e5e8b0aaafedda258747817e5fcd68468edc0f85
PARIZ\ASUS <pariz1813491@gmail.com>
2022-12-06 10:40:04 +0700

    FE Terbaru

--- src/pages/history/index.jsx
+++ src/pages/history/index.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Navbar from "../../components/navbar";
 import CardHistory from "../../components/card-history";
+import Navigation from "../../components/header/Navigation";
 
 const History = () => {
 
   return (
     <>
       <div className="bg-utama">
-        <Navbar />
-        <div className="md:pt-20"></div>
+        <Navigation/>
+        <div className=" md:pt-16"></div>
         <CardHistory />
       </div>
     </>
